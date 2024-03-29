c91e35eba17b335a03bf53d03083cfb1d32c124e
PREM NARAYAN SHARMA <143782425+premsharma8168@users.noreply.github.com>
2024-03-30 00:04:37 +0530

    Add files via upload

---
+++ ZYX/Int.java
@@ -0,0 +1,14 @@
+public class Int {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 10;
+        int sum = a + b;
+        int diff = a - b;
+        int mul = a * b;
+        // int div = a / b;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(mul);
+        // System.out.println(div);
+    }
+}
