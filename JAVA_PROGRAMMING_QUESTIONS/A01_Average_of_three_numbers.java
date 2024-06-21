8c4122604ce5d0bcad07bcce3ff1d8f8f79638eb
PREM NARAYAN SHARMA <143782425+premsharma8168@users.noreply.github.com>
2024-06-21 21:40:31 +0530

    Delete JAVA_PROGRAMMING_QUESTIONS directory

--- JAVA_PROGRAMMING_QUESTIONS/A01_Average_of_three_numbers.java
+++
@@ -1,9 +0,0 @@
-public class A01_Average_of_three_numbers {
-  public static void main(String[] args) {
-    int a = 132;
-    int b = 123;
-    int c = 231;
-    int Average = (a + b + c) / 3;
-    System.out.println(Average);
-  }
-}
